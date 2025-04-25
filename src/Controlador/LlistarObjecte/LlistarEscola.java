fefb6661ac9f551b67ec40dcdcbca6069c24b8ea
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2025-04-25 08:36:27 +0200

    Llistar Escola i Sector implementat

--- src/Controlador/LlistarObjecte/LlistarEscola.java
+++ src/Controlador/LlistarObjecte/LlistarEscola.java
@@ -1,4 +1,33 @@
 package Controlador.LlistarObjecte;
 
+import Model.Constructors.Escola;
+import Model.DAO.EscolaDAO;
+import Vista.Vista;
+
+import java.util.Scanner;
+
 public class LlistarEscola {
+    public static void llistarEscola() {
+        Scanner scanner = new Scanner(System.in);
+
+        Vista.mostrarMissatge("Digues el nom de l'escola:");
+        String nom = scanner.nextLine();
+
+        EscolaDAO escolaDAO = new EscolaDAO();
+
+        int id = escolaDAO.obtenirPerNom(nom);
+        Escola escola = escolaDAO.obtenir(id);
+
+        if (escola != null) {
+            Vista.mostrarMissatge("Informació de l'escola:");
+            Vista.mostrarMissatge("Nom: " + escola.getNom());
+            Vista.mostrarMissatge("ID Localització: " + escola.getIdLocalitzacio());
+            Vista.mostrarMissatge("Aproximació: " + escola.getAproximacio());
+            Vista.mostrarMissatge("Nombre de vies: " + escola.getNumeroVies());
+            Vista.mostrarMissatge("Popularitat: " + escola.getPopularitat());
+            Vista.mostrarMissatge("Restriccions: " + escola.getRestriccions());
+        } else {
+            Vista.mostrarMissatge("Escola no trobada.");
+        }
+    }
 }
--- src/Controlador/LlistarObjecte/LlistarSector.java
+++ src/Controlador/LlistarObjecte/LlistarSector.java
@@ -1,4 +1,34 @@
 package Controlador.LlistarObjecte;
 
+import Model.Constructors.Sector;
+import Model.DAO.SectorDAO;
+import Vista.Vista;
+
+import java.util.Scanner;
+
 public class LlistarSector {
+    public static void llistarSector() {
+        Scanner scanner = new Scanner(System.in);
+
+        Vista.mostrarMissatge("Digues el nom del sector:");
+        String nom = scanner.nextLine();
+
+        SectorDAO sectorDAO = new SectorDAO();
+
+        int id = sectorDAO.obtenirPerNom(nom);
+        Sector sector = sectorDAO.obtenir(id);
+
+        if (sector != null) {
+            Vista.mostrarMissatge("Informació del sector:");
+            Vista.mostrarMissatge("Nom: " + sector.getNom());
+            Vista.mostrarMissatge("Coordenades: " + sector.getCoordenades());
+            Vista.mostrarMissatge("Aproximació: " + sector.getAproximacio());
+            Vista.mostrarMissatge("Nombre de vies: " + sector.getNumeroVies());
+            Vista.mostrarMissatge("Popularitat: " + sector.getPopularitat());
+            Vista.mostrarMissatge("Restriccions: " + sector.getRestriccions());
+            Vista.mostrarMissatge("ID Escola: " + sector.getIdEscola());
+        } else {
+            Vista.mostrarMissatge("Sector no trobat.");
+        }
+    }
 }
