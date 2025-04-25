1cfd3ca8c841db27a41e96457b4da103fcef6ceb
Samuel Cañadas Ruiz <samuelcanadas2711@gmail.com>
2025-04-25 09:46:53 +0200

    Interface CRUD actualitzada amb obtenirPerNom

--- src/Model/Interfaces/CRUD.java
+++ src/Model/Interfaces/CRUD.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface CRUD<T> {
     void inserir(T obj);
     T obtenir(int id);
-    List<T> obtenirTots();
+    List<T> llistarTots();
     void actualitzar(T obj);
     void eliminar(int id);
     int obtenirPerNom(String nom);
